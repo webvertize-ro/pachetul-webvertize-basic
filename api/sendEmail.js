f905682525c0d968fb00f7576613d59f0a033e8c
webvertize-ro <contact@webvertize.ro>
2026-01-26 03:56:42 +0200

    implementing form

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -3,8 +3,9 @@ export default function handler(req, res) {
     return res.status(400).json({ status: 'Method not allowed!' });
   }
 
-  const body = req.body;
-  console.log('received data is: ', body);
+  console.log('received data is: ', req.body);
 
-  res.status(200).json({ success: true, message: 'Received data', data: body });
+  res
+    .status(200)
+    .json({ success: true, message: 'Received data', data: req.body });
 }
