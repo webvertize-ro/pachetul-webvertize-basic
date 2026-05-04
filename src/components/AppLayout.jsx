be27857b2d03a0df7391c91375fd8f42fefd35cd
webvertize-ro <contact@webvertize.ro>
2026-05-04 15:38:33 +0300

    further changes

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,26 +1,25 @@
-import Footer from './Footer';
-import { Outlet } from 'react-router';
-import Navigation from './Navigation';
-import SideButtons from './SideButtons';
-import BackTopBtn from './BackTopBtn';
-import CookiePopup from './CookiePopup';
-import BottomNav from './BottomNav';
-import { Helmet } from 'react-helmet-async';
-import { c } from '../utils/content';
-import { useContent } from '../hooks/useContent';
+import Footer from "./Footer";
+import { Outlet } from "react-router";
+import Navigation from "./Navigation";
+import SideButtons from "./SideButtons";
+import BackTopBtn from "./BackTopBtn";
+import CookiePopup from "./CookiePopup";
+import BottomNav from "./BottomNav";
+import { Helmet } from "react-helmet-async";
+import { c } from "../utils/content";
+import { useContent } from "../hooks/useContent";
 
 function AppLayout() {
   const { contentMap } = useContent();
-  console.log('console.log output: ', c(contentMap, 'global.favicon'));
 
   return (
     <div>
       <Helmet>
-        {c(contentMap, 'global.favicon') && (
+        {c(contentMap, "global.favicon") && (
           <link
             rel="icon"
             type="image/png"
-            href={c(contentMap, 'global.favicon')}
+            href={c(contentMap, "global.favicon")}
           />
         )}
       </Helmet>
